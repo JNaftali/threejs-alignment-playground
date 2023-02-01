f640933a3906a6a463846a2346067bb3d7e1d2cb
Josh Marantz <jmarantz@thelabnyc.com>
2023-02-01 14:22:13 -0500

    refactor

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,17 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const loader = new GLTFLoader();
 
-declare var scene: THREE.Scene;
-declare var camera: THREE.Camera;
-scene = new THREE.Scene();
-camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+declare global {
+	interface Window {
+		scene: THREE.Scene;
+		camera: THREE.Camera;
+	}
+}
+
+const scene = new THREE.Scene();
+window.scene = scene;
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+window.camera = camera;
 camera.position.z = 10;
 
 const axesHelper = new THREE.AxesHelper(5);
@@ -17,22 +24,28 @@ const light = new THREE.PointLight();
 light.position.set(0, 10, 10);
 scene.add(light);
 
-loader.load("/3d/Square_Rosette.gltf", (gltf) => {
-	camera.lookAt(gltf.scene.position);
-	const group = new THREE.Group();
-	for (let obj of gltf.scene.children) {
-		if (isMesh(obj)) {
-			group.add(obj);
-		} else {
-			console.log("Unknown object type: ", obj.type);
-		}
-	}
-	scene.add(group);
-});
+loadModel("Square_Rosette");
+loadModel("Coventry_Lever_newUVs");
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+function loadModel(name: string) {
+	loader.load(`/3d/${name}.gltf`, (gltf) => {
+		camera.lookAt(gltf.scene.position);
+		const group = new THREE.Group();
+		group.name = name;
+		for (let obj of gltf.scene.children) {
+			if (isMesh(obj)) {
+				group.add(obj);
+			} else {
+				console.log("Unknown object type: ", obj.type);
+			}
+		}
+		scene.add(group);
+	});
+}
 
 function animate() {
 	requestAnimationFrame(animate);
