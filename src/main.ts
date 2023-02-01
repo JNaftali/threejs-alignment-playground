94648ff99b21f6805e9aa7e9646e2c72d6c1fc55
Josh Marantz <jmarantz@thelabnyc.com>
2023-02-01 14:16:07 -0500

    tweak

--- src/main.ts
+++ src/main.ts
@@ -4,25 +4,20 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const loader = new GLTFLoader();
 
-const scene = new THREE.Scene();
-(window as any).scene = scene;
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-(window as any).camera = camera;
-// const geometry = new THREE.BoxGeometry(1, 1, 1);
-// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-// const cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
+declare var scene: THREE.Scene;
+declare var camera: THREE.Camera;
+scene = new THREE.Scene();
+camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 10;
+
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
+
 const light = new THREE.PointLight();
 light.position.set(0, 10, 10);
 scene.add(light);
 
-camera.position.z = 10;
-
-const renderer = new THREE.WebGLRenderer();
 loader.load("/3d/Square_Rosette.gltf", (gltf) => {
-	(window as any).gltf = gltf;
 	camera.lookAt(gltf.scene.position);
 	const group = new THREE.Group();
 	for (let obj of gltf.scene.children) {
@@ -34,6 +29,8 @@ loader.load("/3d/Square_Rosette.gltf", (gltf) => {
 	}
 	scene.add(group);
 });
+
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
