dbce0982128f37a04ddcac4f6bb3400b95e9caba
Josh Marantz <jmarantz@thelabnyc.com>
2023-02-02 12:36:22 -0500

    so cloooooooose

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import "./style.css";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { AmbientLight } from "three";
 
 const loader = new GLTFLoader();
 
@@ -13,33 +14,70 @@ const scene = new THREE.Scene();
 window.scene = scene;
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 window.camera = camera;
-camera.position.z = 10;
+camera.position.set(35, 25, -90);
 const controls = new OrbitControls(camera, renderer.domElement);
 
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
 const light = new THREE.PointLight();
-light.position.set(0, 10, 10);
+light.position.set(0, -100, -100);
 scene.add(light);
+scene.add(new AmbientLight());
 
-loadModel("Square_Rosette");
-loadModel("Coventry_Lever_newUVs");
+const door = await loadModel("doorTest");
+const handle = await loadModel("handleTest");
+const rosette = await loadModel("rosetteTest");
+
+// Move rosette onto door
+const [door_mount_point, door_mount_quat] = getWorldPos(door.getObjectByName("null")!);
+const [rosette_door_mount_point, rosette_door_mount_quat] = getWorldPos(
+	rosette.getObjectByName("null_door")!
+);
+const DVquat = door_mount_quat.multiply(rosette_door_mount_quat.invert());
+rosette.applyQuaternion(DVquat);
+const DRvec = door_mount_point.sub(rosette_door_mount_point);
+rosette.translateX(DRvec.x);
+rosette.translateY(DRvec.y);
+rosette.translateZ(DRvec.z);
+
+// Move handle onto rosette
+const [rosette_handle_mount_point, rosette_handle_mount_quat] = getWorldPos(
+	rosette.getObjectByName("null_Rosette")!
+);
+const [handle_mount_point, handle_mount_quat] = getWorldPos(handle.getObjectByName("null_Handle")!);
+const RHquat = handle_mount_quat.multiply(rosette_handle_mount_quat.invert());
+handle.applyQuaternion(RHquat);
+const RHvec = rosette_handle_mount_point.sub(handle_mount_point);
+handle.translateX(RHvec.x);
+handle.translateY(RHvec.y);
+handle.translateZ(RHvec.z);
+
+function getWorldPos(thing: THREE.Object3D) {
+	thing.updateMatrixWorld();
+	const vec = new THREE.Vector3();
+	const quat = new THREE.Quaternion();
+	thing.getWorldPosition(vec);
+	thing.getWorldQuaternion(quat);
+	return [vec, quat] as const;
+}
 
 function loadModel(name: string) {
-	loader.load(`/3d/${name}.gltf`, (gltf) => {
+	return loader.loadAsync(`/3d/${name}.gltf`).then((gltf) => {
+		scene.add(gltf.scene);
+		gltf.scene.name = name;
 		camera.lookAt(gltf.scene.position);
 		controls.update();
-		const group = new THREE.Group();
-		group.name = name;
-		for (let obj of gltf.scene.children) {
-			if (isMesh(obj)) {
-				group.add(obj);
-			} else {
-				console.log("Unknown object type: ", obj.type);
-			}
-		}
-		scene.add(group);
+		return gltf.scene;
+		// const group = new THREE.Group();
+		// group.name = name;
+		// for (let obj of gltf.scene.children) {
+		// 	if (isMesh(obj)) {
+		// 		group.add(obj);
+		// 	} else {
+		// 		console.log("Unknown object type: ", obj.type);
+		// 	}
+		// }
 	});
 }
 
